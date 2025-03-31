c55bcaf68aee6d0b6f007dff5a1798d69f3be438
redeemednorabbi <redeemednotquitearabbi@gmail.com>
2025-03-31 16:26:57 -0400

    Module 6 assignement 1

---
+++ Exam.java
@@ -0,0 +1,45 @@
+public class Exam extends Assessment {
+
+    private int questions;
+    private int missed;
+
+    //constructor
+    public Exam(int questions, int missed){
+        setQuestions(questions);
+        setMissed(missed);
+
+        scoreCalc();
+    }
+
+    // Setter methods
+    public void setQuestions(int newQuestions){
+        this.questions = newQuestions;
+    }
+    public void setMissed(int newMissed){
+        this.missed = newMissed;
+    }
+
+    // Other methods.
+
+    public void scoreCalc(){
+        int questionScore = getQuestions();
+        int missedScore = getNumMissed();
+
+        int pointsPerQuestion = getPointsEach();
+        int finalScore = pointsPerQuestion * (questionScore-missedScore);
+        setScore(finalScore);
+    }
+
+    public int getQuestions(){
+        return questions;
+    }
+
+    public int getNumMissed(){
+        return missed;
+    }
+
+    public int getPointsEach(){
+        return 100/getQuestions();
+
+    }
+}
